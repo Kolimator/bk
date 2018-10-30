135d6d40f6476c4745de5b94ad050f5b16f180fb
Kolimator <karol.nowak@vip,onet.pl>
2018-10-30 11:34:34 +0100

    prawie ready szablon

---
+++ main.js
@@ -0,0 +1,8 @@
+function KittyPaw() {
+    let container = document.querySelector(".content")
+let paw = document.createElement("i")
+ let classPaw =   paw.classList.add("kittyPaw ")
+   console.log( container.appendChild(classPaw))
+}
+
+KittyPaw();
